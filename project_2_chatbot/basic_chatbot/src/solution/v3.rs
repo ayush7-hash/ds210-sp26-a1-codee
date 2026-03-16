85c472bd2d76b8360fc34f6ad64d690d6997ccf2
nataliezyip <nyip@bu.edu>
2026-03-16 19:56:01 -0400

    st2 implemented v3

--- project_2_chatbot/basic_chatbot/src/solution/v3.rs
+++ project_2_chatbot/basic_chatbot/src/solution/v3.rs
@@ -1,38 +1,55 @@
 use kalosm::language::*;
+use std::collections::HashMap;
 
 #[allow(dead_code)]
 pub struct ChatbotV3 {
-    // What should you store inside your Chatbot type?
-    // The model? The chat_session?
-    // Storing a single chat session is not enough: it mixes messages from different users
-    // together!
-    // Need to store one chat session per user.
-    // Think of some kind of data structure that can help you with this.
+    // Store the model so new chat sessions can be created
+    model: Llama,
+
+    // Map each username to their own chat session
+    chats: HashMap<String, Chat<Llama>>,
 }
 
 impl ChatbotV3 {
     #[allow(dead_code)]
     pub fn new(model: Llama) -> ChatbotV3 {
-        return ChatbotV3 {
-            // Make sure you initialize your struct members here
-        };
+        ChatbotV3 {
+            model,
+            chats: HashMap::new(),
+        }
     }
 
     #[allow(dead_code)]
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-        // Add your code for chatting with the agent while keeping conversation history here.
-        // Notice, you are given both the `message` and also the `username`.
-        // Use this information to select the correct chat session for that user and keep it
-        // separated from the sessions of other users.
-        return String::from("Hello, I am not a bot (yet)!");
+        // If this is the first message from the user, create a new chat session
+        if !self.chats.contains_key(&username) {
+            let chat = self.model.chat();
+            self.chats.insert(username.clone(), chat);
+        }
+
+        // Retrieve the correct chat session for this user
+        let chat = self.chats.get_mut(&username).unwrap();
+
+        // Add the user's message and generate a response
+        let response = chat.add_message(message).await.unwrap();
+
+        response
     }
 
     #[allow(dead_code)]
     pub fn get_history(&self, username: String) -> Vec<String> {
-        // Extract the chat message history for the given username
-        // Hint: think of how you can retrieve the Chat object for that user, when you retrieve it
-        // you may want to use https://docs.rs/kalosm/0.4.0/kalosm/language/struct.Chat.html#method.session
-        // to then retrieve the history!
-        return Vec::new();
+        // Retrieve the chat history for the given user
+        if let Some(chat) = self.chats.get(&username) {
+            if let Ok(session) = chat.session() {
+                return session
+                    .history()
+                    .iter()
+                    .map(|msg| format!("{:?}: {}", msg.role(), msg.content()))
+                    .collect();
+            }
+        }
+
+        // Return empty history if the user has no chat session yet
+        Vec::new()
     }
 }
