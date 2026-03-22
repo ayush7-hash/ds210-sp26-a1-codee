b8f9c3d094988dff3972b6cd139b54daf4143e27
ayush <ayush7@bu.edu>
2026-03-22 19:05:47 -0400

    Final V4

--- project_2_chatbot/file_chatbot/src/solution/v4.rs
+++ project_2_chatbot/file_chatbot/src/solution/v4.rs
@@ -14,29 +14,36 @@ impl ChatbotV4 {
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
     let filename = &format!("{}.txt", username);
+    let filename = &format!("{}.txt", username);
 
     let mut chat_session: Chat<Llama> = self.model
         .chat()
         .with_system_prompt("The assistant will act like a pirate");
+    let mut chat_session: Chat<Llama> = self.model
+        .chat()
+        .with_system_prompt("The assistant will act like a pirate");
 
-    // Load previous session if it exists
+    // Load previous session
     match file_library::load_chat_session_from_file(&filename) {
-        None => {
-            // No previous session → use fresh chat_session
-        }
+        None => {}
         Some(session) => {
             chat_session = chat_session.with_session(session);
         }
     }
 
-    // Send message and get response
+    // Get response
     let response = chat_session
         .add_message(message)
         .await
-        .map(|msg| format!("{:?}: {}" , msg.role() , msg.content()));
+        .expect("Failed to generate response");
 
-    // Save updated session
-    file_library::save_chat_session_to_file(&filename, chat_session.session());
+    // Get session
+    let session = chat_session
+        .session()
+        .expect("Failed to get session");
+
+    // Save session
+    file_library::save_chat_session_to_file(&filename, &session);
 
     return response;
 }
