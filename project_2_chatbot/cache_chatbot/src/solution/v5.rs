35084b192004cb6970c8015509497e22f2c6fc48
nataliezyip <nyip@bu.edu>
2026-03-23 21:39:03 -0400

    sst2 commit v5 containing chat_with_user

--- project_2_chatbot/cache_chatbot/src/solution/v5.rs
+++ project_2_chatbot/cache_chatbot/src/solution/v5.rs
@@ -10,49 +10,57 @@ pub struct ChatbotV5 {
 
 impl ChatbotV5 {
     pub fn new(model: Llama) -> ChatbotV5 {
-        return ChatbotV5 {
-            model: model,
+        ChatbotV5 {
+            model,
             cache: Cache::new(3),
-        };
+        }
     }
 
     pub async fn chat_with_user(&mut self, username: String, message: String) -> String {
-        let filename = &format!("{}.txt", username);
-        let cached_chat = self.cache.get_chat(&username);
+        let filename: &String = &format! ("{}.txt", username);
+        let cached_chat: Option<&mut Chat<Llama>> = self. cache.get_chat (&username);
 
         match cached_chat {
             None => {
-                println!("chat_with_user: {username} is not in the cache!");
-                // The cache does not have the chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
+                println! ("chat_with_user: {username} is not in the cache!");
+                // since the convo is not in the cache, we can check if its in the file
+                // try to load previous conversation
+                let mut chat: Chat<Llama> =
+                if let Some(session) = file_library:: load_chat_session_from_file(filename) {
+                    println! ("Loaded {username) from file");
+                // rebuild the chat
+                    self.model.chat().with_session(session)
+                }
+                // no file found, new user
+                else {
+                    println! ("Creating new chat for {username}");
+                    self.model.chat()
+                };
+                // add the message to the conversation, and comes up with a response
+                let response: String = chat.add_message (message).await.unwrap().to_string();
+                // save the session to the file
+                // &chat.session.unwrap() here because mismatched types
+                file_library::save_chat_session_to_file(filename,  &chat.session().unwrap());
+                //put into the cache
+                self.cache.insert_chat(username, chat);
+                return response;
             }
-            Some(chat_session) => {
-                println!("chat_with_user: {username} is in the cache! Nice!");
-                // The cache has this chat. What should you do?
-                return String::from("Hello, I am not a bot (yet)!");
-
-            }
+        Some (chat_session) => {
+            println! ("chat_with_user: {username} is in the cache! Nice!");
+            // the chat is already in memory
+            // add the message to the conversation, and comes up with a response
+            let response: String = chat_session.add_message(message).await.unwrap().to_string();
+            // save the convo to the file
+            // save the session to the file
+            // &chat.session.unwrap() here because mismatched types
+            file_library::save_chat_session_to_file(filename, &chat_session.session().unwrap());
+            return response;
         }
-    }
+    } 
+}
+
 
     pub fn get_history(&mut self, username: String) -> Vec<String> {
-        let filename = &format!("{}.txt", username);
-        let cached_chat = self.cache.get_chat(&username);
-
-        match cached_chat {
-            None => {
-                println!("get_history: {username} is not in the cache!");
-                // TODO: The cache does not have the chat. What should you do?
-                // Your code goes here.
-                return Vec::new();
-            }
-            Some(chat_session) => {
-                println!("get_history: {username} is in the cache! Nice!");
-                // TODO: The cache has this chat. What should you do?
-                // Your code goes here.
-                return Vec::new();
-
-            }
-        }
+        self.cache.get_history(&username)
     }
 }
