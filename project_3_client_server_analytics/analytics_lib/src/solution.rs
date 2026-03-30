86e15e09303f1230f611ddf837045ac8762947d8
ayush <ayush7@bu.edu>
2026-03-30 03:52:38 -0400

    part 1 changes

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,12 +2,50 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
+pub fn evaluate_condition(row: &Row, condition: &Condition, dataset: &Dataset) -> bool {
+        match condition {
+            Condition::Equal(col, val) => {
+                let index = dataset.column_index(col);
+                row.get_value(index) == val
+            }
+            Condition::Not(inner) => {
+                !evaluate_condition(row, inner, dataset)
+            }
+            Condition::And(left, right) => {
+                evaluate_condition(row, left, dataset) && evaluate_condition(row, right, dataset)
+            }
+            Condition::Or(left, right) => {
+                evaluate_condition(row, left, dataset) || evaluate_condition(row, right, dataset)
+            }
+        }
+    }
 pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
-    todo!("Implement this!");
+    let mut filtered = Dataset::new(dataset.columns().clone());
+
+    for row in dataset.iter() {
+        if evaluate_condition(row, filter, dataset) {
+            filtered.add_row(row.clone());
+        }
+    }
+
+    filtered
 }
 
 pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
-    todo!("Implement this!");
+    let group_index = dataset.column_index(group_by_column);;
+    let mut grouped_data: HashMap<Value, Dataset> = HashMap::new();
+
+    for row in dataset.iter() {
+        let key = row.get_value(group_index).clone();;
+
+        if !grouped_data.contains_key(&key) {
+            grouped_data.insert(key.clone(), Dataset::new(dataset.columns().clone()));
+        }
+
+        grouped_data.get_mut(&key).unwrap().add_row(row.clone());
+    }
+
+    grouped_data
 }
 
 pub fn aggregate_dataset(dataset: HashMap<Value, Dataset>, aggregation: &Aggregation) -> HashMap<Value, Value> {
